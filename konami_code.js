48f6227c58f8dfbe854ef1beb51f67490d792987
Asaf Davidov <adavidov@conncoll.edu>
2018-05-08 22:42:31 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -17,26 +17,26 @@ function init() {
   const body = document.querySelector('body');
   body.addEventListener('keydown', onKeyDownHandler());
   function onKeyDownHandler(e){
-  
-  if (e === undefined){
-    console.log('loaded');
-  }
-  else{
     
-    const key = e.key;
-    console.log(key);
-    //if (key === codes[index]){
-      //index++;
-      //if (index === codes.length){
-        //alert("Hurray!");
+    if (e === undefined){
+      console.log('loaded');
+    }
+    else{
+      
+      const key = e.key;
+      console.log(key);
+      //if (key === codes[index]){
+        //index++;
+        //if (index === codes.length){
+          //alert("Hurray!");
+          //index = 0;
+        //}
+      //}
+      //else{
         //index = 0;
       //}
-    //}
-    //else{
-      //index = 0;
-    //}
+    }
   }
-}
 }
 
 
