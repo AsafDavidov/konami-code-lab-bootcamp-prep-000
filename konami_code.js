1aff32bfd1235a0c89aeaa7c6e1f3cba43515592
Asaf Davidov <adavidov@conncoll.edu>
2018-05-08 22:32:06 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,26 @@ const codes = [
   "b",
   "a"
 ];
+let index = 0;
 
+function onKeyDownHandler(e){
+  if (e === undefined){}
+  else{
+    
+    const key = e.key;
+    console.log(key);
+    if (key === codes[index]){
+      index++;
+      if (index === codes.length){
+        alert("Hurray!");
+        index = 0;
+      }
+    }
+    else{
+      index = 0;
+    }
+  }
+}
 
 function init() {
   const body = document.querySelector('body');
